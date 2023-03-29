4ecb215947ace7cb88dbe47eb73c9121e57d8b29
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-03-29 16:11:43 +0530

    Datatypes in C Language

---
+++ Class_Codes/DataTypes/Datatypes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){                         // Void - Void also a Datatype and its size is 0/1
+ 	                             
+	                             // Datatype    Size       Format Specifier
+	int age = 45;                // Integer   - 4 Bytes   - %d
+	float price = 67.89;         // Float     - 4 Bytes   - %f
+	double salary = 200.837469;  // Double    - 8 Bytes   - %lf
+	char grade = 'A';            // Character - 1 Bytes   - %c
+	
+       printf("Age : %d\n",age);	       //45
+       printf("Price : %f\n",price);    //67.889999
+       printf("Salary : %lf\n",salary); //200.837469	
+       printf("Grade : %c\n",grade);    //A		
+}	
