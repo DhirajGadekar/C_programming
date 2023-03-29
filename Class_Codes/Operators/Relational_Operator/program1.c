342414ae194284169d8afa5b0b8c165e8b91dee3
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-03-29 16:16:27 +0530

    Relational Operators in C Language

---
+++ Class_Codes/Operators/Relational_Operator/program1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+	
+	int x = 6;
+	int y = 7;
+
+	printf("%d\n",x == y);    //0
+	printf("%d\n",x < y);     //1
+	printf("%d\n",x > y);     //0
+	printf("%d\n",x <= y);    //1
+	printf("%d\n",x >= y);    //0
+	printf("%d\n",x != y);    //1
+}	
