6947ec38e731f2800533d585cb4522e814fa964d
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-03-30 22:09:26 +0530

    Break and Continue in C

---
+++ Class_Codes/BreakAndContinue/Break.c
@@ -0,0 +1,19 @@
+//Break
+
+
+#include<stdio.h>
+
+void main() {
+
+	for(int i = 1;i <= 10;i++){
+		
+		if(i < 5){
+			printf("%d\n",i);
+
+		}else{
+	
+			break;
+		}
+
+	}
+}
---
+++ Class_Codes/BreakAndContinue/continue.c
@@ -0,0 +1,18 @@
+//Continue
+
+
+#include<stdio.h>
+
+void main() {
+
+	for(int i = 1;i <= 10;i++){
+		
+		if(i == 5){
+			
+			continue;
+		}else{
+
+			printf("%d\n",i);
+		}
+	}
+}
